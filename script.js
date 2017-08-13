dfef1f5fa6d5b7069065ea9ee9f9ee05491976b1
Aakash Sardana <aakashsardana3@gmail.com>
2017-08-14 00:28:42 +0530

    Apply selected class to the clicked button

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
 easyBtn.addEventListener("click", function () {
-	
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
 });
 
 hardBtn.addEventListener("click", function () {
-	
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
 });
 
 resetButton.addEventListener("click", function () {
