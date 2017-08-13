b95adce04a9b968e768a4c9c0de5ad314a01be34
Aakash Sardana <aakashsardana3@gmail.com>
2017-08-13 22:58:16 +0530

    Change background of h1 when right color is clicked

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ for (var i = 0; i < squares.length; i++) {
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again!"; 
