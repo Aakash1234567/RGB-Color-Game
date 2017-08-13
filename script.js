430131887067228f35b5249c2b63f4871400ff3d
Aakash Sardana <aakashsardana3@gmail.com>
2017-08-13 23:55:46 +0530

    Add event listener to the reset button

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.getElementById('message');
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function () {
+	colors = generateRandomColors(numColors);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+	}
+});
 
 colorDisplay.textContent = pickedColor;
 
